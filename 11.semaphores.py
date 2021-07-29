288c429a1f0e900f67d6660e3461438ab40d6fad
HARRY <harisenkumar1997@gmail.com>
2021-07-29 09:50:59 +0530

    61th commit semaphore  sala 2 time lga diya tha mcd

---
+++ 11.semaphores.py
@@ -0,0 +1,29 @@
+from threading import *
+class Flight:
+    def __init__(self, available_seat):
+        self.available_seat = available_seat
+        self.l = BoundedSemaphore(2)
+        print(self.l)
+        
+    def reserve(self, need_seat):
+        self.l.acquire()
+        self.l.acquire()
+        print(self.l._value)
+        print('Available Seat:', self.available_seat)
+        if(self.available_seat>= need_seat):
+            name = current_thread().name
+            print(f'{need_seat} seat is alloted for {name}')
+            self.available_seat -= need_seat
+        else:
+            print('Sorry! All seats has alloted')
+
+        self.l.release()
+        self.l.release()
+
+f= Flight(2)
+t1 = Thread(target= f.reserve, args=(1,), name='Rahu')
+t2 = Thread(target= f.reserve, args=(1,), name='Sona')
+t3 = Thread(target= f.reserve, args=(1,), name='Hari')
+t1.start()
+t2.start()
+t3.start()
